2633d22fb4a23066224b98c4ad3582d773d2da9b
CIHIKU <gewpm10@gmail.com>
2023-09-19 16:33:39 -0400

    Changed CryptoHelper from static to interface for mocking.

--- AuthService/Helpers/CryptoHelper.cs
+++ AuthService/Helpers/CryptoHelper.cs
@@ -3,33 +3,33 @@ using System.Text;
 
 namespace AuthService.Helpers;
 
-public static class CryptoHelper
+public class CryptoHelper : ICryptoHelper
 {
-    private static readonly byte[] AesKey;
+    private readonly byte[] _aesKey;
 
-    private static readonly byte[] AesIv;
+    private readonly byte[] _aesIv;
 
-    static CryptoHelper()
+    public CryptoHelper()
     {
-        AesKey = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("AES_KEY") ?? "MySecretKey_1234");
-        AesIv = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("AES_IV") ?? "MySecretIV__1234");
+        _aesKey = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("AES_KEY") ?? "MySecretKey_1234");
+        _aesIv = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("AES_IV") ?? "MySecretIV__1234");
         
-        if (AesKey.Length is not (16 or 24 or 32))
+        if (_aesKey.Length is not (16 or 24 or 32))
         {
             throw new Exception("Invalid AES key size. It must be 16, 24, or 32 bytes.");
         }
 
-        if (AesIv.Length != 16)
+        if (_aesIv.Length != 16)
         {
             throw new Exception("Invalid AES IV size. It must be 16 bytes.");
         }
     }
     
-    public static string Encrypt(string plainText)
+    public string Encrypt(string plainText)
     {
         using var aes = Aes.Create();
-        aes.Key = AesKey;
-        aes.IV = AesIv;
+        aes.Key = _aesKey;
+        aes.IV = _aesIv;
 
         var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 
@@ -44,11 +44,11 @@ public static class CryptoHelper
         return Convert.ToBase64String(ms.ToArray());
     }
 
-    public static string Decrypt(string cipherText)
+    public string Decrypt(string cipherText)
     {
         using var aes = Aes.Create();
-        aes.Key = AesKey;
-        aes.IV = AesIv;
+        aes.Key = _aesKey;
+        aes.IV = _aesIv;
 
         var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
 
